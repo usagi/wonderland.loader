0f28a8472068b3b6f46065008c876f60d891cfc4
Usagi Ito <usagi@WonderRabbitProject.net>
2015-01-14 22:32:16 +0900

    curl --> PocoNet

--- include/wonder_rabbit_project/wonderland/loader.detail/native.hxx
+++ include/wonder_rabbit_project/wonderland/loader.detail/native.hxx
@@ -32,30 +32,35 @@ namespace wonder_rabbit_project
             // temporary buffer
             buffer_t buffer;
             
-            try
+            auto retry = 8;
+            
+            while( retry-- )
             {
-              
-              Poco::URI uri( url );
+              try
+              {
+                Poco::URI uri( url );
 
-              Poco::Net::HTTPClientSession session( uri.getHost(), uri.getPort() );
-              
-              const auto path = uri.getPathAndQuery();
-              Poco::Net::HTTPRequest request( Poco::Net::HTTPRequest::HTTP_GET, path.empty() ? "/" : path, Poco::Net::HTTPMessage::HTTP_1_1 );
-              
-              session.sendRequest( request );
-              
-              Poco::Net::HTTPResponse response;
-              
-              if ( response.getStatus() != Poco::Net::HTTPResponse::HTTPStatus::HTTP_OK )
-                return buffer;
-              
-              auto& bin = session.receiveResponse( response );
-              std::copy( std::istreambuf_iterator< char >( bin ), std::istreambuf_iterator< char >(), std::back_inserter( buffer ) );
-            }
-            catch( const Poco::Exception& e )
-            {
-              std::cerr << "== Poco Exception ==> " << e.what();
-              throw e;
+                Poco::Net::HTTPClientSession session( uri.getHost(), uri.getPort() );
+                
+                const auto path = uri.getPathAndQuery();
+                Poco::Net::HTTPRequest request( Poco::Net::HTTPRequest::HTTP_GET, path.empty() ? "/" : path, Poco::Net::HTTPMessage::HTTP_1_1 );
+                
+                session.sendRequest( request );
+                
+                Poco::Net::HTTPResponse response;
+                
+                if ( response.getStatus() != Poco::Net::HTTPResponse::HTTPStatus::HTTP_OK )
+                  return buffer;
+                
+                auto& bin = session.receiveResponse( response );
+                std::copy( std::istreambuf_iterator< char >( bin ), std::istreambuf_iterator< char >(), std::back_inserter( buffer ) );
+                break;
+              }
+              catch( const Poco::Exception& e )
+              {
+                std::cerr << "== Poco Exception ==> " << e.what();
+                buffer.clear();
+              }
             }
 
             return buffer;
